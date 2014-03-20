c6af21e86433c34874e0264922cecbaac7428fae
David Monk <velowander@users.noreply.github.com>
2014-03-20 13:46:45 +0800

    Dates work now (stored internally as longs Int64Date) Refactoring: eliminate old messy KidsWorldNtag203 class which was procedural and difficult to debug. KidsCard now encapsulates functionality to read and write on its own

--- KidsWorld/src/main/java/com/dip/kidsworld/SimpleDebitActivity.java
+++ KidsWorld/src/main/java/com/dip/kidsworld/SimpleDebitActivity.java
@@ -71,16 +71,14 @@ public class SimpleDebitActivity extends Activity {
         /* Read or write (depending on button_mode) from the NFC card
         * For this sample application, deducts the constant DEBIT_AMOUNT from */
         ToggleButton button_mode = (ToggleButton) findViewById(R.id.button_mode);
+        KidsCard kidsCard = new KidsCard(intent);
         if (button_mode.isChecked()) {
             //Debit mode
             Log.d(LOG_TAG, "Debit Tag mode");
-            int balance = KidsWorldNtag203.readTagBalance(intent);
-            boolean writeOk = false;
-            KidsCard kidsCard = new KidsCard(balance);
             if (kidsCard.buy(new Product())) {
-                writeOk = KidsWorldNtag203.writeTag(intent, kidsCard);
+                boolean writeOk = kidsCard.write();
                 if (writeOk) {
-                    UpdateUI(intent);
+                    UpdateUI(kidsCard);
                     Toast.makeText(this, "Debit complete", Toast.LENGTH_LONG).show();
                 } else
                     Toast.makeText(this, "Unable to process transaction", Toast.LENGTH_LONG).show();
@@ -89,65 +87,54 @@ public class SimpleDebitActivity extends Activity {
             else Toast.makeText(this, "Insufficient balance!", Toast.LENGTH_LONG).show();
         } else {
             //Read mode
-            UpdateUI(intent);
+            UpdateUI(kidsCard);
         }
     }
 
-    public void UpdateUI(final Intent intent) {
+    public void UpdateUI(final KidsCard kidsCard) {
         //intent - pass from onNewIntent, presence of NFC tag
         Log.d(LOG_TAG, "Update UI from read");
-        KidsCard gateEvent = KidsWorldNtag203.readTag(intent);
         try {
-            TextView tvwId = (TextView) findViewById(R.id.client_id);
-            tvwId.setText(String.valueOf(gateEvent.id));
+            TextView tvwClientId = (TextView) findViewById(R.id.client_id);
+            tvwClientId.setText(String.valueOf(kidsCard.clientId));
         } catch (Exception e) {
             Log.e(LOG_TAG, "UpdateUI Exception", e);
         }
         try {
             TextView tvwName = (TextView) findViewById(R.id.client_name);
-            tvwName.setText(gateEvent.name);
+            tvwName.setText(kidsCard.name);
         } catch (Exception e) {
             Log.e(LOG_TAG, "UpdateUI Exception", e);
         }
         try {
             TextView tvwBalance = (TextView) findViewById(R.id.balance);
-            tvwBalance.setText(String.valueOf(gateEvent.getBalance() / 100.));
+            tvwBalance.setText(String.valueOf(kidsCard.getBalance() / 100.));
         } catch (Exception e) {
             Log.e(LOG_TAG, "UpdateUI Exception", e);
         }
         try {
             TextView tvwDebit = (TextView) findViewById(R.id.last_purchase);
-            Date date = new Utils.Int64Date(gateEvent.getLastPurchaseTicks());
-            tvwDebit.setText(date.toString());
+            tvwDebit.setText(kidsCard.getLastPurchaseDate().toString());
         } catch (Exception e) {
             Log.e(LOG_TAG, "UpdateUI Exception", e);
         }
         try {
             TextView tvwCheckIn = (TextView) findViewById(R.id.check_in);
-            Log.d(LOG_TAG, "SimpleDebitActivity.UpdateUI(); checkInTicks: " + gateEvent.getCheckInTicks());
-            Date date = new Utils.Int64Date(gateEvent.getCheckInTicks());
-            tvwCheckIn.setText(date.toString());
+            tvwCheckIn.setText(kidsCard.getCheckInDate().toString());
         } catch (Exception e) {
             Log.e(LOG_TAG, "UpdateUI Exception", e);
         }
         try {
             TextView tvwCheckOut = (TextView) findViewById(R.id.check_out);
-            Log.d(LOG_TAG, "SimpleDebitActivity.UpdateUI(); checkOutTicks: " + gateEvent.getCheckOutTicks());
-            Date date = new Utils.Int64Date(gateEvent.getCheckOutTicks());
-            tvwCheckOut.setText(date.toString());
-            Toast.makeText(this, "Read complete", Toast.LENGTH_LONG).show();
+            tvwCheckOut.setText(kidsCard.getCheckOutDate().toString());
         } catch (Exception e) {
             Log.e(LOG_TAG, "UpdateUI Exception", e);
         }
+        Toast.makeText(this, "Read complete", Toast.LENGTH_LONG).show();
     }
 
-    @Deprecated
-    public static class KidsWorldNtag203 {
-        /* Helper class for reading and writing Kids World data to/from Mifare Ultralight card
-        * The code is specific to the Mifare Ultralight which has 4 byte pages, reading from
-        * the card returns 4 pages at a time. To store bytes compatible with Visual C++, it
-        * reverses the byte order of both reads and writes. */
-        private static final String LOG_TAG = KidsWorldNtag203.class.getSimpleName();
+    public static class KidsCard {
+        private static final String LOG_TAG = KidsCard.class.getSimpleName();
 
         //Constants for reading and writing to Mifare NTAG203
         private static final short OFFSET_ID = 4;
@@ -156,62 +143,46 @@ public class SimpleDebitActivity extends Activity {
         private static final short OFFSET_CHECKIN = 14;// Uses all 4 pages for 2 dates (stored as doubles)
         private static final short OFFSET_DEBIT_TIME = 19;
 
-        public static int readTagBalance(final Intent intent) {
-            /*Reads only the balance from the card, doesn't get all the fields */
-            Tag myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            MifareUltralight mifare = MifareUltralight.get(myTag);
+        private Intent nfcIntent;
 
-            int balance = -1;
-            try {
-                mifare.connect();
-                balance = ByteBuffer.wrap(mifare.readPages(OFFSET_BALANCE)).order(Utils.Ntag203.ENDIANNESS).getInt();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    mifare.close();
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "readTagBalance couldn't close mifare!!");
-                }
-            }
-            Log.d(LOG_TAG, "balance: " + balance);
+        private int clientId;
+        private String name;
+        private int balance;
+        private Utils.Int64Date lastPurchaseDate;
+        private Utils.Int64Date checkInDate;
+        private Utils.Int64Date checkOutDate;
 
-            return balance;
-        }
-
-        public static KidsCard readTag(final Intent intent) {
+        public KidsCard(Intent intent) {
             Log.d(LOG_TAG, "Native Byte Ordering is Little Endian? " + String.valueOf(ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN));
+            this.nfcIntent = intent;
             Tag myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             MifareUltralight mifare = MifareUltralight.get(myTag);
             try {
                 mifare.connect();
                 int id = ByteBuffer.wrap(mifare.readPages(OFFSET_ID)).order(Utils.Ntag203.ENDIANNESS).getInt();
-                Log.d(LOG_TAG, "id: " + id);
+                Log.d(LOG_TAG, "clientId: " + id);
+                this.clientId = id;
 
                 String name = new String(mifare.readPages(OFFSET_NAME)); //pages 6-9
                 Log.d(LOG_TAG, "name: " + name);
+                this.name = name;
 
                 int balance = ByteBuffer.wrap(mifare.readPages(OFFSET_BALANCE)).order(Utils.Ntag203.ENDIANNESS).getInt();
                 Log.d(LOG_TAG, "balance: " + balance);
-
-                ByteBuffer dateBuffer = ByteBuffer.wrap(mifare.readPages(OFFSET_CHECKIN)).order(Utils.Ntag203.ENDIANNESS);
-                long checkInTicks = dateBuffer.getLong();
-                Log.d(LOG_TAG, "checkInTicks (long): " + checkInTicks);
-
-                long checkOutTicks = dateBuffer.getLong();
-                Log.d(LOG_TAG, "checkOutTicks (long): " + checkOutTicks);
+                this.balance = balance;
 
                 long debitTicks = ByteBuffer.wrap(mifare.readPages(OFFSET_DEBIT_TIME)).order(Utils.Ntag203.ENDIANNESS).getLong();
                 Log.d(LOG_TAG, "debitTicks (long): " + debitTicks);
+                this.lastPurchaseDate = new Utils.Int64Date(debitTicks);
 
-                KidsCard kidsCard = new KidsCard(balance);
-                kidsCard.id = id;
-                kidsCard.name = name;
-                kidsCard.checkInTicks = checkInTicks;
-                kidsCard.checkOutTicks = checkOutTicks;
-
-                return kidsCard;
-
+                //Read check in and check out together since they are adjacent
+                ByteBuffer dateBuffer = ByteBuffer.wrap(mifare.readPages(OFFSET_CHECKIN)).order(Utils.Ntag203.ENDIANNESS);
+                long checkInTicks = dateBuffer.getLong();
+                Log.d(LOG_TAG, "checkInTicks (long): " + checkInTicks);
+                this.checkInDate = new Utils.Int64Date(checkInTicks);
+                long checkOutTicks = dateBuffer.getLong();
+                Log.d(LOG_TAG, "checkOutTicks (long): " + checkOutTicks);
+                this.checkOutDate = new Utils.Int64Date(checkOutTicks);
             } catch (IOException e) {
                 Log.e(LOG_TAG, "IOException while writing MifareUltralight message...", e);
             } catch (NullPointerException e) {
@@ -225,36 +196,54 @@ public class SimpleDebitActivity extends Activity {
                     }
                 }
             }
-            return null;
         }
 
-        public static boolean writeTag(final Intent intent, final KidsCard kidsCard) {
+        public boolean buy(Product product) {
+            int price = product.getPrice();
+            if (balance >= price) {
+                balance -= price;
+                lastPurchaseDate = new Utils.Int64Date();
+                return true;
+            }
+            return false;
+        }
+
+        public int getClientId() {
+            return this.clientId;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+
+        public Utils.Int64Date getCheckInDate() {
+            /* Returns the check in time in Intel64 ticks */
+            return checkInDate;
+        }
+
+        public Utils.Int64Date getCheckOutDate() {
+            /* Returns the check out time in Intel64 ticks */
+            return checkOutDate;
+        }
+
+        public Utils.Int64Date getLastPurchaseDate() {
+            /* Returns the last purchase time in Intel64 ticks */
+            return lastPurchaseDate;
+        }
+
+        public boolean write() {
             /* intent - pass onNewIntent from scanning NFC tag, saves kidsCard fields to card
             * times are stored as longs using Int64 conversion for .NET compatibility (Int64Date class)
             * Note: many fields are commented out as only the Windows .NET version modifies them */
-            Tag myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Tag myTag = nfcIntent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             MifareUltralight ultralight = MifareUltralight.get(myTag);
 
-            //if (kidsCard.name.length() > 16) { throw new IllegalArgumentException("Max name length: 16"); }
-            //pad name to fixed length of 16 bytes, makes it easier to encode
-            //String namePadded = padRight(kidsCard.name, ' ' , 16);
-            //Log.d(LOG_TAG, "namePadded length: " + namePadded.length());
-            //Log.d(LOG_TAG, "namePadded:<string>" + namePadded + "</string>" );
-            //byte[] name06 = namePadded.substring( 0, 4).getBytes(Charset.forName(myCharset));
-            //byte[] name07 = namePadded.substring( 4, 8).getBytes(Charset.forName(myCharset));
-            //byte[] name08 = namePadded.substring( 8,12).getBytes(Charset.forName(myCharset));
-            //byte[] name09 = namePadded.substring(12,16).getBytes(Charset.forName(myCharset));
             try {
                 ultralight.connect();
-                //ultralight.writePage(4, ByteBuffer.allocate(4).order(KW_ENDIANNESS).putInt(kidsCard.id).array());
-                //ultralight.writePage( 6, name06);
-                //ultralight.writePage( 7, name07);
-                //ultralight.writePage( 8, name08);
-                //ultralight.writePage( 9, name09);
-                ultralight.writePage(OFFSET_BALANCE, ByteBuffer.allocate(4).order(Utils.Ntag203.ENDIANNESS).putInt(kidsCard.balance).array());
-                Log.d(LOG_TAG, "Saving balance of " + kidsCard.getBalance());
+                ultralight.writePage(OFFSET_BALANCE, ByteBuffer.allocate(4).order(Utils.Ntag203.ENDIANNESS).putInt(this.balance).array());
+                Log.d(LOG_TAG, "Saving balance of " + getBalance());
                 Date date = new Utils.Int64Date();
-                Log.d(LOG_TAG, "writeTag: date is: " + date.toString());
+                Log.d(LOG_TAG, "write: date is: " + date.toString());
                 Utils.Ntag203.writeLong(ultralight, new Utils.Int64Date().getTicks(), OFFSET_DEBIT_TIME);
                 Log.d(LOG_TAG, "Saved Debit Ticks (long): " + String.valueOf(date.getTime()));
                 Log.d(LOG_TAG, "Saved Debit Time: " + date.toString());
@@ -270,52 +259,6 @@ public class SimpleDebitActivity extends Activity {
                 }
             }
             return false;
-        }
-    }
-
-    public static class KidsCard {
-        private int id;
-        private String name;
-        private int balance;
-        private long lastPurchaseTicks;
-        private long checkInTicks;
-        private long checkOutTicks;
-
-        public KidsCard(int balance) {
-            this.balance = balance;
-        }
-
-        public boolean buy(Product product) {
-            int price = product.getPrice();
-            if (balance >= price) {
-                balance -= price;
-                lastPurchaseTicks = new Utils.Int64Date().getTicks();
-                return true;
-            }
-            return false;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public int getBalance() {
-            return balance;
-        }
-
-        public long getCheckInTicks() {
-            /* Returns the check in time in Intel64 ticks */
-            return checkInTicks;
-        }
-
-        public long getCheckOutTicks() {
-            /* Returns the check out time in Intel64 ticks */
-            return checkOutTicks;
-        }
-
-        public long getLastPurchaseTicks() {
-            /* Returns the last purchase time in Intel64 ticks */
-            return lastPurchaseTicks;
         }
     }
 
